4651909521c048d778ab6cc76b992aa1f208dbcd
Kunal <33kunalg@gmail.com>
2022-01-08 08:47:50 +0530

    modified topics

--- public/script.js
+++ public/script.js
@@ -157,18 +157,3 @@ export default function Profile() {
 
 export default function TodoList() {
   return (
-    // This doesn't quite work!
-    <h1>Hedy Lamarr's Todos</h1>
-    <img
-      src="https://i.imgur.com/yXOvdOSs.jpg"
-      alt="Hedy Lamarr"
-      class="photo"
-    >
-    <ul>
-      <li>Invent new traffic lights
-      <li>Rehearse a movie scene
-      <li>Improve spectrum technology
-    </ul>
-  );
-}
-
