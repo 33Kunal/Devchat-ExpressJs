8c3cd0ca8418d4f830fee2009d7e1c7bb91df607
Kunal <33kunalg@gmail.com>
2023-01-13 08:47:50 +0530

    modified topics

--- public/script.js
+++ public/script.js
@@ -190,3 +190,13 @@ function Cup() {
   guest = guest + 1;
   return <h2>Tea cup for guest #{guest}</h2>;
 }
+export default function TeaSet() {
+  return (
+    <>
+      <Cup />
+      <Cup />
+      <Cup />
+    </>
+  );
+}
+
