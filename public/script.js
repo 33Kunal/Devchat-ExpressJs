c7185815711ad7c3de0a727c1bed872cd4e57d7a
Kunal <33kunalg@gmail.com>
2023-03-03 08:47:50 +0530

    modified topics

--- public/script.js
+++ public/script.js
@@ -220,4 +220,14 @@ export default function Button() {
     </button>
   );
 }
+export default function Button() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
+  return (
+    <button onClick={handleClick}>
+      Click me
+    </button>
+  );
+}
 
