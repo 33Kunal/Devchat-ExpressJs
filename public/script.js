45e2869bab05e93a77b7c1da319ce3d1531a14f2
Kunal <33kunalg@gmail.com>
2023-04-04 08:47:50 +0530

    modified topics

--- public/script.js
+++ public/script.js
@@ -246,3 +246,15 @@ function AlertButton({ message, children }) {
     </button>
   );
 }
+export default function Toolbar() {
+  return (
+    <div>
+      <AlertButton message="Playing!">
+        Play Movie
+      </AlertButton>
+      <AlertButton message="Uploading!">
+        Upload Image
+      </AlertButton>
+    </div>
+  );
+}
