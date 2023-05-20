82288c1870808d21b09e9760b7e48cdb09063300
Kunal <33kunalg@gmail.com>
2023-05-20 08:47:50 +0530

    modified topics

--- public/script.js
+++ public/script.js
@@ -269,5 +269,11 @@ function PlayButton({ movieName }) {
   function handlePlayClick() {
     alert(`Playing ${movieName}!`);
   }
+  return (
+    <Button onClick={handlePlayClick}>
+      Play "{movieName}"
+    </Button>
+  );
+}
 
 
