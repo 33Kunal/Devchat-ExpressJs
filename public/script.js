56450a89cf69df388b61ce2488b164325b6964fa
Kunal <33kunalg@gmail.com>
2023-05-28 08:47:50 +0530

    modified topics

--- public/script.js
+++ public/script.js
@@ -297,6 +297,18 @@ function Button({ onSmash, children }) {
     </button>
   );
 }
+export default function App() {
+  return (
+    <div>
+      <Button onSmash={() => alert('Playing!')}>
+        Play Movie
+      </Button>
+      <Button onSmash={() => alert('Uploading!')}>
+        Upload Image
+      </Button>
+    </div>
+  );
+}
 
 
 
