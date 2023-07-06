eb4719ebf433e8ac2ace0a18feaef31f10db07a0
Kunal <33kunalg@gmail.com>
2023-07-06 08:47:50 +0530

    modified topics

--- public/script.js
+++ public/script.js
@@ -334,6 +334,22 @@ function Button({ onClick, children }) {
   );
 }
 
+export default function Toolbar() {
+  return (
+    <div className="Toolbar" onClick={() => {
+      alert('You clicked on the toolbar!');
+    }}>
+      <Button onClick={() => alert('Playing!')}>
+        Play Movie
+      </Button>
+      <Button onClick={() => alert('Uploading!')}>
+        Upload Image
+      </Button>
+    </div>
+  );
+}
+
+
 
 
 
